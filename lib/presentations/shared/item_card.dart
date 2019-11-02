415d694518a433a598c7c3f7a247ecae622ad462
naimishverma17 <itakgec@gmail.com>
2019-11-02 19:24:11 +0530

    changed items card

--- lib/presentations/shared/item_card.dart
+++ lib/presentations/shared/item_card.dart
@@ -9,70 +9,61 @@ class ItemCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 180.0,
-      child: Stack(
+    return Card(
+        child: Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Row(
         children: <Widget>[
-          Positioned(
-            top: 20.0,
-            left: 20.0,
-            right: 0.0,
+          Expanded(
+            flex: 1,
             child: Container(
-              height: 140.0,
-              child: Card(
-                  elevation: 5.0,
-                  child: Container(
-                    padding: const EdgeInsets.only(top: 10.0, right: 20.0),
-                    margin: const EdgeInsets.only(left: 130.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text(
-                          item.title,
-                          style: Styles.pageTitleText(),
-                        ),
-                        SizedBox(
-                          height: 5.0,
-                        ),
-                        Text(
-                          item.minQuantity,
-                          style: TextStyle(
-                              color: Styles.subHeadingColor,
-                              fontSize: 16.0,
-                              fontWeight: FontWeight.w600),
-                        ),
-                        SizedBox(
-                          height: 15.0,
-                        ),
-                        Text(
-                          'Rs. ${item.price}',
-                          style: TextStyle(
-                              color: Styles.subHeadingColor,
-                              fontSize: 16.0,
-                              fontWeight: FontWeight.w600),
-                        ),
-                      ],
-                    ),
-                  )),
+              height: 120.0,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(3.0),
+                image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: NetworkImage(item.imageUrl),
+                ),
+              ),
             ),
           ),
-          Align(
-            alignment: Alignment.topLeft,
+          Expanded(
+            flex: 2,
             child: Container(
-              height: 130.0,
-              width: 130.0,
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                    image: NetworkImage('https://media.istockphoto.com/photos/vegetable-basket-picture-id511378964?k=6&m=511378964&s=612x612&w=0&h=qt3SJVjM-_hikHxDKlTsgSzgNUOoxKc9DZ0qZ791tNQ='),
-                    fit: BoxFit.cover),
-                borderRadius: BorderRadius.all(
-                  Radius.circular(8.0),
-                ),
+              margin: EdgeInsets.only(left: 30.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    item.title,
+                    style: Styles.pageTitleText(),
+                  ),
+                  SizedBox(
+                    height: 5.0,
+                  ),
+                  Text(
+                    item.minQuantity,
+                    style: TextStyle(
+                        color: Styles.subHeadingColor,
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.w600),
+                  ),
+                  SizedBox(
+                    height: 15.0,
+                  ),
+                  Text(
+                    'Rs. ${item.price}',
+                    style: TextStyle(
+                        color: Styles.subHeadingColor,
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.w600),
+                  ),
+                ],
               ),
             ),
           )
         ],
       ),
-    );
+    ));
   }
 }
