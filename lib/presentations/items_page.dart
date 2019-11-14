20a65969125b43e711d734d6ca60fcc831cd22b7
naimishverma17 <itakgec@gmail.com>
2019-11-14 17:28:46 +0530

    search chnages

--- lib/presentations/items_page.dart
+++ lib/presentations/items_page.dart
@@ -44,43 +44,45 @@ class _ItemsPage extends State<ItemsPage> {
         backgroundColor: Colors.white,
         iconTheme: IconThemeData(color: Styles.primaryColor),
       ),
-      body: _isLoading
-          ? Center(
-              child: CircularProgressIndicator(),
-            )
-          : Container(
-              decoration: BoxDecoration(color: Styles.homeBackgroundColor),
-              child: Padding(
-                padding: const EdgeInsets.symmetric(
-                    horizontal: 15.0, vertical: 18.0),
-                child: Column(
-                  children: <Widget>[
-                    SearchBar(
-                      search: _searchItems,
-                    ),
-                    SizedBox(
-                      height: 10.0,
-                    ),
-                    _items.length == 0
-                        ? Center(
-                            child: Text(
-                              'Sorry, It seems no product exist with this name!',
-                              style: Styles.labelText(),
-                            ),
-                          )
-                        : Expanded(
-                            child: ListView.builder(
-                                itemCount: _items.length,
-                                itemBuilder: (context, index) {
-                                  return ItemCard(
-                                    _items[index],
-                                  );
-                                }),
-                          )
-                  ],
-                ),
+      body: Container(
+        decoration: BoxDecoration(color: Styles.homeBackgroundColor),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 18.0),
+          child: Column(
+            children: <Widget>[
+              SearchBar(
+                initialValue: _keyword,
+                search: _searchItems,
               ),
-            ),
+              SizedBox(
+                height: 10.0,
+              ),
+              _isLoading
+                  ? Center(
+                      child: CircularProgressIndicator(),
+                    )
+                  : Container(
+                      child: _items.length == 0
+                          ? Center(
+                              child: Text(
+                                'Sorry, It seems no product exist with this name!',
+                                style: Styles.labelText(),
+                              ),
+                            )
+                          : Expanded(
+                              child: ListView.builder(
+                                  itemCount: _items.length,
+                                  itemBuilder: (context, index) {
+                                    return ItemCard(
+                                      _items[index],
+                                    );
+                                  }),
+                            ),
+                    ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 
@@ -104,6 +106,7 @@ class _ItemsPage extends State<ItemsPage> {
   }
 
   _searchItems(String keyword) async {
+    _keyword = keyword;
     try {
       setState(() {
         _isLoading = true;
